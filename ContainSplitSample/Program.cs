f526a75f5ec0f2044de861f1e4df86e1a6ecaea3
hainesma <hainesma@gmail.com>
2021-05-28 13:29:06 -0400

    Add project files.

---
+++ ContainSplitSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ContainSplitSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            string sample = "some words here";
+
+            string[] splitSamples = sample.Split('w');
+
+            // Print out the elements of splitSample array
+            foreach (string element in splitSamples) {
+                Console.WriteLine(element);
+            }
+
+            string s1 = "The quick brown fox jumps over the lazy dog";
+            string s2 = "fox";
+
+            bool hasInIt = s1.Contains(s2);
+            Console.WriteLine(hasInIt);
+        
+
+        }
+
+        
+    }
+}
